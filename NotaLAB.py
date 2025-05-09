9fd1849e5f71cc22883c467a89c9e2ac82b484e1
GuilllasDefas <guilhermedifreitas@gmail.com>
2025-05-08 21:49:13 -0300

    Função pra carregar o audio

--- NotaLAB.py
+++ NotaLAB.py
@@ -2,5 +2,14 @@ import librosa
 import numpy as np
 
 from spleeter.separator import Separator # vai servir para separar os instrumentos (voz, baixo, bateria e outros)
-from music21 import stream, note, chord
+from music21 import stream, note, chord # para manipular as notas musicais
+
+def carregar_audio(caminho, sr=44100):
+    '''
+    # Carrega um arquivo de áudio e retorna o sinal e a taxa de amostragem.
+    sinal = (np.ndarray): série temporal do áudio (amplitudes).
+    taxa = (int): taxa de amostragem do áudio (número de amostras por segundo).
+    '''
+    sinal, taxa = librosa.load(caminho, sr=sr)
+    return sinal, taxa
 
---
+++ README.md
@@ -0,0 +1,26 @@
+
+## Rquerimentos
+
+As dependências do projetos são muito chatas de instalar.
+
+
+```bash
+pip install -r requirements.txt
+```
+
+Ou
+
+pip install librosa
+pip install spleeter
+pip install music21
+
+
+```bash
+py -3.10 -m venv venv
+.\venv\Scripts\Activate.ps1
+```
+
+```bash
+E:/Py_Projetos/Py_Projetos/NotaLAB/venv/Scripts/python.exe -m pip install --upgrade pip setuptools wheel
+```
+
