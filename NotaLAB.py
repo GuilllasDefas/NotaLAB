87b7ccaf362924066f264f0a1c196292353aba73
GuilllasDefas <guilhermedifreitas@gmail.com>
2025-05-08 21:08:29 -0300

    add-requeriments

--- NotaLAB.py
+++ NotaLAB.py
@@ -1,1 +1,6 @@
 import librosa
+import numpy as np
+
+from spleeter.separator import Separator # vai servir para separar os instrumentos (voz, baixo, bateria e outros)
+from music21 import stream, note, chord
+
